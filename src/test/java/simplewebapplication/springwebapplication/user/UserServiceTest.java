eeb5402a260e6fba1723d98ff9bd0950851c4d00
jh <lyn00227@naver.com>
2021-02-01 16:33:27 +0900

    유저 테스트

--- src/test/java/simplewebapplication/springwebapplication/user/UserServiceTest.java
+++ src/test/java/simplewebapplication/springwebapplication/user/UserServiceTest.java
@@ -35,4 +35,20 @@ public class UserServiceTest {
         System.out.println("savedId = " + savedId);
         System.out.println("findUser = " + findUser.getId());
     }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+
+        // given
+        User user1 = new User("user01", "123123", "lyn00227@naver.com");
+        //User user2 = new User("user01", "123123", "lyn00227@naver.com");
+        User user2 = new User("user02", "123123", "lyn0227@nate.com");
+
+        // when
+        userService.join(user1);
+        userService.join(user2); // 같은 이름이므로 예외가 발생해야 한다!!!!!
+
+        // then
+        fail("예외가 발생해야 한다."); // 코드가 돌다 여기에 오면 잘못됐음을 알림.
+    }
 }
