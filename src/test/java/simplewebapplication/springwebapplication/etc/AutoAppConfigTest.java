707ed333fe9574253488f7b7e0dcf50da95beda5
jh <lyn00227@naver.com>
2021-02-02 14:52:49 +0900

    UserService 스프링 컨테이너 등록 확인 테스트

---
+++ src/test/java/simplewebapplication/springwebapplication/etc/AutoAppConfigTest.java
@@ -0,0 +1,19 @@
+package simplewebapplication.springwebapplication.etc;
+
+import org.junit.jupiter.api.Test;
+import simplewebapplication.springwebapplication.SpringWebApplication;
+import simplewebapplication.springwebapplication.service.user.UserService;
+import org.assertj.core.api.Assertions;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SpringWebApplication.class);
+
+        // UserService 가 스프링 컨테이너에 등록 되었는지 확인
+        UserService userService = ac.getBean(UserService.class);
+        Assertions.assertThat(userService).isInstanceOf(UserService.class);
+    }
+}
