13f94e3bacb19827104267872280fb106e77abc1
jh <lyn00227@naver.com>
2021-02-05 18:44:04 +0900

    서비스 스프링 컨테이너 등록 확인 테스트

--- src/test/java/simplewebapplication/springwebapplication/etc/AutoAppConfigTest.java
+++ src/test/java/simplewebapplication/springwebapplication/etc/AutoAppConfigTest.java
@@ -2,6 +2,7 @@ package simplewebapplication.springwebapplication.etc;
 
 import org.junit.jupiter.api.Test;
 import simplewebapplication.springwebapplication.SpringWebApplication;
+import simplewebapplication.springwebapplication.service.board.BoardService;
 import simplewebapplication.springwebapplication.service.user.UserService;
 import org.assertj.core.api.Assertions;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,5 +16,9 @@ public class AutoAppConfigTest {
         // UserService 가 스프링 컨테이너에 등록 되었는지 확인
         UserService userService = ac.getBean(UserService.class);
         Assertions.assertThat(userService).isInstanceOf(UserService.class);
+
+        // BoardService 가 스프링 컨테이너에 등록 되었는지 확인
+        BoardService boardService = ac.getBean(BoardService.class);
+        Assertions.assertThat(boardService).isInstanceOf(BoardService.class);
     }
 }
