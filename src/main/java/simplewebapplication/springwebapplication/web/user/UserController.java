4d86d66c54cc74a64804521b13b6715eaaeaa9ac
jh <lyn00227@naver.com>
2021-07-31 18:42:51 +0900

    회원가입 검증 기능 수정 BindingResult

--- src/main/java/simplewebapplication/springwebapplication/web/user/UserController.java
+++ src/main/java/simplewebapplication/springwebapplication/web/user/UserController.java
@@ -5,10 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import simplewebapplication.springwebapplication.domain.user.User;
 import simplewebapplication.springwebapplication.web.form.UserJoinForm;
 import simplewebapplication.springwebapplication.web.form.UserLoginForm;
@@ -81,44 +84,52 @@ public class UserController {
 
     // 회원가입
     @PostMapping("/join")
-    public String checkUserJoinV1(@ModelAttribute UserJoinForm joinInfo, Model model) {
+    public String checkUserJoin(@ModelAttribute(name = "joinInfo") UserJoinForm joinInfo, BindingResult bindingResult,
+                                RedirectAttributes redirectAttributes, Model model)
+    {
 
-        Map<String, String> errors = new HashMap<>();
+        //Map<String, String> errors = new HashMap<>();
 
         // 검증 로직
 
         // 아이디 확인
         if (!StringUtils.hasText(joinInfo.getUserId())) {
             log.info("유저 아이디 = {}", joinInfo.getUserId());
-            errors.put("userId", "회원 아이디는 필수 입니다.");
+            //errors.put("userId", "회원 아이디는 필수 입니다.");
+            bindingResult.addError(new FieldError("joinInfo", "userId", "회원 아이디는 필수 입니다."));
         }
         if (7 > joinInfo.getUserId().length() || joinInfo.getUserId().length() > 15) {
             log.info("유저 아이디 글자수 = {}", joinInfo.getUserId().length());
-            errors.put("userId", "아이디의 글자 수는 7자 이상 15자 이하 입니다.");
+            //errors.put("userId", "아이디의 글자 수는 7자 이상 15자 이하 입니다.");
+            bindingResult.addError(new FieldError("joinInfo", "userId", "아이디의 글자 수는 7자 이상 15자 이하 입니다."));
         }
 
         // 비밀번호 확인
         if (!StringUtils.hasText(joinInfo.getPassword()) || !StringUtils.hasText(joinInfo.getRePassword())) {
             log.info("유저 비번 = {}", joinInfo.getPassword());
             log.info("유저 재비번 = {}", joinInfo.getRePassword());
-            errors.put("rePassword", "회원 비밀 번호는 필수 입니다.");
+            //errors.put("rePassword", "회원 비밀 번호는 필수 입니다.");
+            bindingResult.addError(new FieldError("joinInfo", "rePassword", "회원 비밀번호는 필수 입니다."));
         } else if (!joinInfo.getPassword().equals(joinInfo.getRePassword())) {
-            errors.put("rePassword", "입력하신 비밀번호가 일치하지 않습니다.");
+            //errors.put("rePassword", "입력하신 비밀번호가 일치하지 않습니다.");
+            bindingResult.addError(new FieldError("joinInfo", "rePassword", "입력하신 비밀번호가 일치하지 않습니다."));
         }
 
         // 이메일 확인
         if (!StringUtils.hasText(joinInfo.getEmail())) {
             log.info("유저 이메일 = {}", joinInfo.getEmail());
-            errors.put("userEmail", "회원 이메일은 필수 입니다.");
+            //errors.put("userEmail", "회원 이메일은 필수 입니다.");
+            bindingResult.addError(new FieldError("joinInfo", "email", "회원 이메일은 필수 입니다."));
         }
         else if (!isValidEmail(joinInfo.getEmail())) {
-            errors.put("userEmail", "유효한 이메일 형식이 아닙니다.");
+            //errors.put("userEmail", "유효한 이메일 형식이 아닙니다.");
+            bindingResult.addError(new FieldError("joinInfo", "email", "유효한 이메일 형식이 아닙니다."));
         }
 
         // 검증 실패 시 입력 폼으로 되돌림
-        if (!errors.isEmpty()) {
-            log.info("errors = {}", errors);
-            model.addAttribute("errors", errors);
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            //model.addAttribute("errors", errors);
             return "users/sign_up";
         }
 
