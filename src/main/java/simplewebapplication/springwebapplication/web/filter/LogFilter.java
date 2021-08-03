4eb34c370b672a36d55795400c9b86367e323901
jh <lyn00227@naver.com>
2021-08-03 19:20:50 +0900

    필터 클래스 생성

---
+++ src/main/java/simplewebapplication/springwebapplication/web/filter/LogFilter.java
@@ -0,0 +1,47 @@
+package simplewebapplication.springwebapplication.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+
+        // 다운 캐스팅
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        String requestURI = httpRequest.getRequestURI();
+
+        log.info("LoginCheckFilter 내가 요청한 URI = {}", requestURI);
+
+        // 요청마다 사용자를 구분
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
---
+++ src/main/java/simplewebapplication/springwebapplication/web/filter/LoginCheckFilter.java
@@ -0,0 +1,60 @@
+package simplewebapplication.springwebapplication.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+import simplewebapplication.springwebapplication.web.SessionConst;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Slf4j
+public class LoginCheckFilter implements Filter {
+
+    private static final String[] whiteList = {"/", "/users/login", "/users/join",
+            "/users/logout", "/boards", "/favicon.ico", "/css/*"};
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        try {
+            log.info("인증 체크 필터 시작 {}", requestURI);
+
+            if (isLoginCheckPath(requestURI)) {
+                log.info("인증 체크 로직 실행 {}", requestURI);
+
+                HttpSession session = httpRequest.getSession(false);
+
+                if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+                    log.info("미인증 사용자 요청 {}", requestURI);
+                    httpResponse.sendRedirect("/users/login?redirectURL=" + requestURI);
+                    return;
+                }
+            }
+
+            chain.doFilter(request, response);
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("인증 체크 필터 종료 {}", requestURI);
+        }
+
+    }
+
+    /**
+     * 화이트 리스트의 경우 인증 체크X
+     * 화이트 리스트에 존재하지 않은 경우 True 를 리턴함
+     */
+    private boolean isLoginCheckPath(String requestURI) {
+        return !PatternMatchUtils.simpleMatch(whiteList, requestURI);
+    }
+}
