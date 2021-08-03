6577ea16fad688378c59bdf6c1c0045a6a1fd8f5
jh <lyn00227@naver.com>
2021-08-03 19:16:33 +0900

    유저 컨트롤러 로그아웃 및 로그인 기능 생성 및 변경

--- src/main/java/simplewebapplication/springwebapplication/web/user/UserController.java
+++ src/main/java/simplewebapplication/springwebapplication/web/user/UserController.java
@@ -8,18 +8,16 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import simplewebapplication.springwebapplication.domain.user.User;
+import simplewebapplication.springwebapplication.web.SessionConst;
 import simplewebapplication.springwebapplication.web.form.UserJoinForm;
 import simplewebapplication.springwebapplication.web.form.UserLoginForm;
 import simplewebapplication.springwebapplication.service.user.UserService;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -33,19 +31,29 @@ public class UserController {
 
     // 로그인 페이지로 이동
     @GetMapping("/login")
-    public String userLogin(Model model) {
-        model.addAttribute("form", new UserLoginForm());
+    public String loginForm(@ModelAttribute("form") UserLoginForm form) {
         return "users/sign_in";
+    }
+
+    // 로그아웃
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return "redirect:/";
     }
 
     // 로그인
     @PostMapping("/login")
-    public String checkUserLogin(@ModelAttribute(name = "form") UserLoginForm form, BindingResult bindingResult,
-                                 RedirectAttributes redirectAttributes, Model model)
+    public String login(@ModelAttribute(name = "form") UserLoginForm form, BindingResult bindingResult,
+                        RedirectAttributes redirectAttributes, Model model,
+                        @RequestParam(defaultValue = "/") String redirectURL,
+                        HttpServletRequest request)
     {
-
-        //Map<String, String> errors = new HashMap<>();
-
         // 검증 로직
 
         // 아이디 기입 확인
@@ -58,23 +66,21 @@ public class UserController {
             return "users/sign_in";
         }
 
-        User user = userService.findUser(form.getId());
+        User loginUser = userService.login(form.getId(), form.getPassword());
 
-        if (user == null || !user.getPassword().equals(form.getPassword())) {
+        if (loginUser == null) {
             log.info("존재하지 않는 회원!!");
-            //errors.put("loginErr", "아이디와 비밀번호를 확인해주세요.");
             bindingResult.addError(new ObjectError("form", "아이디와 비밀번호를 확인해주세요."));
             return "users/sign_in";
         }
 
-//        if (!user.getPassword().equals(form.getPassword())) {
-//            log.info("비밀번호가 틀렸습니다.");
-//            //errors.put("loginErr", "아이디와 비밀번호를 확인해주세요.");
-//            //model.addAttribute("errors", errors);
-//            return "users/sign_in";
-//        }
+        // 로그인 성공 처리
+        // 세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보를 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginUser);
 
-        return "redirect:../";
+        return "redirect:" + redirectURL;
     }
 
     // 회원가입 페이지로 이동
@@ -86,57 +92,62 @@ public class UserController {
 
     // 회원가입
     @PostMapping("/join")
-    public String checkUserJoin(@ModelAttribute(name = "joinInfo") UserJoinForm joinInfo, BindingResult bindingResult,
+    public String checkUserJoin(@ModelAttribute(name = "joinInfo") UserJoinForm form, BindingResult bindingResult,
                                 RedirectAttributes redirectAttributes, Model model)
     {
-
-        //Map<String, String> errors = new HashMap<>();
-
         // 검증 로직
 
         // 아이디 확인
-        if (!StringUtils.hasText(joinInfo.getUserId())) {
-            log.info("유저 아이디 = {}", joinInfo.getUserId());
-            //errors.put("userId", "회원 아이디는 필수 입니다.");
+        if (!StringUtils.hasText(form.getUserId())) {
+            log.info("유저 아이디 = {}", form.getUserId());
             bindingResult.addError(new FieldError("joinInfo", "userId", "회원 아이디는 필수 입니다."));
         }
-        if (7 > joinInfo.getUserId().length() || joinInfo.getUserId().length() > 15) {
-            log.info("유저 아이디 글자수 = {}", joinInfo.getUserId().length());
-            //errors.put("userId", "아이디의 글자 수는 7자 이상 15자 이하 입니다.");
+        if (7 > form.getUserId().length() || form.getUserId().length() > 15) {
+            log.info("유저 아이디 글자수 = {}", form.getUserId().length());
             bindingResult.addError(new FieldError("joinInfo", "userId", "아이디의 글자 수는 7자 이상 15자 이하 입니다."));
         }
 
         // 비밀번호 확인
-        if (!StringUtils.hasText(joinInfo.getPassword()) || !StringUtils.hasText(joinInfo.getRePassword())) {
-            log.info("유저 비번 = {}", joinInfo.getPassword());
-            log.info("유저 재비번 = {}", joinInfo.getRePassword());
-            //errors.put("rePassword", "회원 비밀 번호는 필수 입니다.");
+        if (!StringUtils.hasText(form.getPassword()) || !StringUtils.hasText(form.getRePassword())) {
+            log.info("유저 비번 = {}", form.getPassword());
+            log.info("유저 재비번 = {}", form.getRePassword());
             bindingResult.addError(new FieldError("joinInfo", "rePassword", "회원 비밀번호는 필수 입니다."));
-        } else if (!joinInfo.getPassword().equals(joinInfo.getRePassword())) {
-            //errors.put("rePassword", "입력하신 비밀번호가 일치하지 않습니다.");
+        } else if (!form.getPassword().equals(form.getRePassword())) {
             bindingResult.addError(new FieldError("joinInfo", "rePassword", "입력하신 비밀번호가 일치하지 않습니다."));
         }
 
         // 이메일 확인
-        if (!StringUtils.hasText(joinInfo.getEmail())) {
-            log.info("유저 이메일 = {}", joinInfo.getEmail());
-            //errors.put("userEmail", "회원 이메일은 필수 입니다.");
+        if (!StringUtils.hasText(form.getEmail())) {
+            log.info("유저 이메일 = {}", form.getEmail());
             bindingResult.addError(new FieldError("joinInfo", "email", "회원 이메일은 필수 입니다."));
         }
-        else if (!isValidEmail(joinInfo.getEmail())) {
-            //errors.put("userEmail", "유효한 이메일 형식이 아닙니다.");
+        else if (!isValidEmail(form.getEmail())) {
             bindingResult.addError(new FieldError("joinInfo", "email", "유효한 이메일 형식이 아닙니다."));
         }
 
         // 검증 실패 시 입력 폼으로 되돌림
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
-            //model.addAttribute("errors", errors);
             return "users/sign_up";
         }
 
         // 성공 로직
         // 회원가입 회원정보 저장 로직 미구현
+
+        String joinUser = userService.join(form.createUser());
+
+        log.info("joinUser = {}", joinUser);
+
+        if (joinUser == null) {
+            bindingResult.addError(new FieldError("joinInfo", "userId", "사용할 수 없는 아이디 입니다."));
+        }
+
+        // 검증 실패
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "users/sign_up";
+        }
+
         return "redirect:../";
     }
 
