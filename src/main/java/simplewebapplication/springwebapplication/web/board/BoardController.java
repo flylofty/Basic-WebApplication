d2c48be00fc005b6e3d7edc95cff1647c61f1bf3
jh <lyn00227@naver.com>
2021-08-08 16:26:54 +0900

    게시글 작성 HTML 검증 기능 추가

--- src/main/java/simplewebapplication/springwebapplication/web/board/BoardController.java
+++ src/main/java/simplewebapplication/springwebapplication/web/board/BoardController.java
@@ -4,11 +4,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import simplewebapplication.springwebapplication.domain.user.User;
 import simplewebapplication.springwebapplication.dto.board.ResponseBoard;
+import simplewebapplication.springwebapplication.web.SessionConst;
 import simplewebapplication.springwebapplication.web.form.WriteForm;
 import simplewebapplication.springwebapplication.service.board.BoardService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Slf4j
@@ -44,14 +51,36 @@ public class BoardController {
     }
 
     @GetMapping("/write")
-    public String openWriteForm() {
-        log.info("BoardController.openWriteForm");
+    public String openWriteForm(@ModelAttribute("form") WriteForm form) {
         return "boards/writeForm";
     }
 
     @PostMapping("/write")
-    public String saveWriteForm(WriteForm write) {
-        return "redirect:/boards";
+    public String saveWriteForm(@ModelAttribute(name = "form") WriteForm form, BindingResult bindingResult,
+                                HttpServletRequest request)
+    {
+        // 1. 제목과 내용은 공백일 수 없음 == Validation
+        if (!StringUtils.hasText(form.getTitle())) {
+            bindingResult.addError(new FieldError("form", "title", "제목을 입력해주세요."));
+        }
+
+        if (!StringUtils.hasText(form.getContent())) {
+            bindingResult.addError(new FieldError("form", "content", "내용을 입력해주세요."));
+        }
+
+        if (bindingResult.hasErrors()) {
+            return "boards/writeForm";
+        }
+
+        // 2. 작성자를 UserService 에서 찾아와야함
+        // 세션에 담을 정보를 간소화 해야할 필요가 있음.
+        HttpSession session = request.getSession(false);
+        User sessionUser = (User) session.getAttribute(SessionConst.LOGIN_USER);
+
+        // 3. Board 를 만들고 BoardService 를 통해 새로운 게시글을 만듦
+        Long boardId = boardService.createBoard(form.makeBoard(sessionUser));
+
+        return "redirect:/boards/" + boardId.toString();
     }
 
     @GetMapping("/{boardId}")
--- src/main/java/simplewebapplication/springwebapplication/web/form/WriteForm.java
+++ src/main/java/simplewebapplication/springwebapplication/web/form/WriteForm.java
@@ -2,6 +2,10 @@ package simplewebapplication.springwebapplication.web.form;
 
 import lombok.Getter;
 import lombok.Setter;
+import simplewebapplication.springwebapplication.domain.board.Board;
+import simplewebapplication.springwebapplication.domain.user.User;
+
+import java.time.LocalDateTime;
 
 @Getter @Setter
 public class WriteForm {
@@ -15,5 +19,9 @@ public class WriteForm {
     public WriteForm(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public Board makeBoard(User user) {
+        return new Board(this.title, this.content, user, LocalDateTime.now());
     }
 }
